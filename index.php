37b111de67406c87e4311abe0ee8fce9f5a487ca
Fordenius <childrenrub@gmail.com>
2022-06-04 11:46:38 +0300

    Web site

--- index.php
+++ index.php
@@ -7,19 +7,27 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+<link rel="stylesheet" href="CSS.css">
   </head>
   <body>
-  <p>
+    <header>
+ <h1>Рандомные цитаты</h1>
+</header> 
+ <div class="container">
     <?php
-    
     $numQuote = $_GET['numQuote'];
 
     $sliceOfQuotes = array_slice($quotes, 0, $numQuote);
-
+    shuffle($sliceOfQuotes);
     foreach ($sliceOfQuotes as  $quote) {
       echo "<p>{$quote}</p>";
     }
     ?>
-  </p>
+
+  </div>
+ <footer> 
+   <p class="data">04.06.2022</p>
+ <footer>
+
   </body>
  </html>
