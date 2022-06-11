c1d522616eb451c948f7a0b53e4d72a88afe4ec9
Fordenius <childrenrub@gmail.com>
2022-06-12 00:20:01 +0300

    add-form-choice-number-of-quotes

--- index.php
+++ index.php
@@ -7,21 +7,35 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+<link rel="stylesheet" href="styles.css">
   </head>
   <body>
-  <p>
-    <?php
     
+    <header>
+ <h1>Рандомные цитаты</h1>
+</header> 
+<form>
+<input type="reset" name="sbros" value="Сброс" class="reset"> <br>
+<input type="number" name="numQuote" min="1" max="10" class="colich">
+ <br>
+ <input type="submit" name="button1" value="Вывести" class="button1">
+
+</form>
+ <div class="container">
+    <?php
     $numQuote = $_GET['numQuote'];
 
     $sliceOfQuotes = array_slice($quotes, 0, $numQuote);
-
     shuffle($sliceOfQuotes);
-
     foreach ($sliceOfQuotes as  $quote) {
       echo "<p>{$quote}</p>";
     }
     ?>
-  </p>
+
+  </div>
+ <footer> 
+   <p class="data">04.06.2022</p>
+ <footer>
+
   </body>
  </html>
